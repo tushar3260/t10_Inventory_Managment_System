801d4c7b0ddd329ea0cb06c226fa88572723c66d
Tushar Bansal <tusharbansal3260@gmail.com>
2025-12-04 11:40:19 +0530

    login page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <Router>  
        <Routes>
             <Route path="/" element={<ProductList />} />
+            <Route path="/login" element={<LoginPage />} />
            
           </Routes>
     </Router>
